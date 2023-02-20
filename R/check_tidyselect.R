2dc64831483a81fe501f425520ef0e997a7a14e6
Damiano Oldoni <damiano.oldoni@inbo.be>
2023-02-20 18:52:26 +0100

    Add rename with rlang := !!

--- R/check_tidyselect.R
+++ R/check_tidyselect.R
@@ -2,18 +2,19 @@
 #' evaluation, NSE/SE
 #'
 #' @param df a tibble data.frame with at least two columns: `col_a` and `col_b`
-#'
+#' @param colToChange character with column name to change
 #' @return A tibble data.frame
 #' 
 #' @importFrom dplyr %>% .data
+#' @importFrom rlang !! :=
 #' @export
 #'
 #' @examples
 #' library(dummyr)
-#' check_tidyselect(dummy_df)
-check_tidyselect <- function(df) {
+#' check_tidyselect(dummy_df, "col_c")
+check_tidyselect <- function(df, colToChange) {
   df %>%
     dplyr::filter(.data$col_b %in% c(4, 5)) %>%
     dplyr::select("col_a", "col_c") %>%
-    dplyr::rename(c = "col_c")
+    dplyr::rename(c := !!colToChange)
 }
